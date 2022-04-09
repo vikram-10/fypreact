b8627f684a77820d12634741d01ffd07367d9d80
vikithespider@gmail.com <vikrambalaji2k@gmail.com>
2022-04-09 17:33:57 +0530

    Connected mongoDB

--- fypbe/routes/clientdata.js
+++ fypbe/routes/clientdata.js
@@ -1,31 +1,58 @@
 var express = require('express');
 var router = express.Router();
-const { MongoClient } = require('mongodb');
+const mongo = require('mongodb');
+let mongoClient = mongo.MongoClient;
+let cors=require('cors');
+router.use(cors({
+    origin: '*'
+}));
+let db;
 
-const uri = "mongodb+srv://vikram:Vikram8118@cluster0.6e3ep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://vikram10:vikram2000@cluster0.0rf1v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-    let userWallet=req.body.clientWadress;
-    let walletAdress=userWallet.wadress;
-    console.log(walletAdress);
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-    const collection = client.db("healthchain").collection("registeredUsers");
-    collection.findOne({'wadress':walletAdress}).then(function(doc){
-        if(doc){
-            res.send({
-                "apiStatus":1,
-            });
-        }
-        else{
-            res.send({
-                "apiStatus":0,
-            });
-        }
-    });
-});
+router.post('/', async function(req, res, next) {
+    try{
+        let userWallet=req.body.clientWadress;
+        let walletAdress=userWallet.wadress;
+        let client=await mongoClient.connect(uri);
+        let db=client.db('healthchain');
+        let regUserData=await db.collection("registeredUsers").find({}).toArray();
+        console.log(regUserData);
+    }
+    catch(err){
+        console.log(err);
+    }
+    // console.log(walletAdress);
+//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collectionDb = client.db("healthchain");
+//   const collectionData=collectionDb.collection("registeredUsers");
+//   collectionData.findOne({wadress:walletAdress},function(err, result) {
+//       console.log(result);
+//   });
+//   client.close();
+// });
+//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         console.log("Client Connected! ");
+//         const collection = client.db("healthchain").collection("registeredUsers");
+//     collection.findOne({'wadress':walletAdress}).then(function(doc){
+//         if(doc){
+//             console.log("1");
+//             res.send({
+//                 "apiStatus":1,
+//             });
+//         }
+//         else{
+//             console.log("0");
+//             res.send({
+//                 "apiStatus":0,
+//             });
+//         }
+//     });
+// });
 });
 
 module.exports = router;
